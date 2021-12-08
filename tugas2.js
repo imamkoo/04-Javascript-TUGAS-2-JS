df69af27bce7dae320c9579180909e79938e4d82
Imamkoo <imammm.kooo@gmail.com>
2021-12-09 00:19:36 +0700

    Tugas 2 Javascript

---
+++ tugas2.js
@@ -0,0 +1,19 @@
+var number = 99
+var string = 'ini string'
+
+var boolean = 66
+if (boolean > 44)
+    boolean = true
+else
+    boolean = false
+
+var kosong = null
+var kosong2 = undefined
+var array = [2, 4, 6, 8, 10, 12, 14, 15, 16, 17, 18, ]
+
+console.log(number)
+console.log(string)
+console.log(boolean)
+console.log(kosong)
+console.log(kosong2)
+console.log(array[0])
